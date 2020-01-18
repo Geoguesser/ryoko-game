b021b2c4e8a39464341cc7d8dda33ceef02aa39c
DanceParty <keevandance@gmail.com>
2020-01-17 17:18:10 -0800

    added client package

---
+++ packages/client/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import styles from "./index.module.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className={styles["title"]}>App</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
