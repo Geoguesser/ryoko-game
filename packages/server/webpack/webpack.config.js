d8c356220eb7a58fcf9390d271960707f48ddb94
DanceParty <keevandance@gmail.com>
2020-01-17 18:12:18 -0800

    add server package

---
+++ packages/server/webpack/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path")
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+  entry: path.resolve(__dirname, "../index.ts"),
+  output: {
+    path: path.resolve(__dirname, "../build"),
+    filename: "index.js"
+  },
+  target: "node",
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        oneOf: [
+          {
+            test: /\.tsx?$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"]
+              }
+            }
+          },
+        ]
+      }
+    ]
+  }
+}
