37171c4d922f1f91eae5bc86f2b42303b991fc4d
DanceParty <keevandance@gmail.com>
2020-01-17 19:26:03 -0800

    Setup prettier

---
+++ packages/server/src/utils/url.ts
@@ -0,0 +1,7 @@
+export const clientUrl =
+  process.env.NODE_ENV === 'production' ? 'https://www.ryoko.gg' : 'http://localhost:3000'
+
+export const googleCallbackUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://ryoko-dev.herokuapp.com/auth/google/redirect'
+    : 'http://localhost:5000/auth/google/redirect'
