a462e6d2fefb914738096bf47b972a3610acc4b6
DanceParty <keevandance@gmail.com>
2020-01-17 19:21:32 -0800

    work in progress

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,15 +1,22 @@
+require("dotenv").config({ path: "../../.env" });
+
 import express from "express"
+import { setupSession } from "./src/setup";
 
-const { PORT = 5000 } = process.env;
+function initialize(): void {
+  const app = express();
+  setupSession(app)
+}
 
-const app = express();
+// const { PORT = 5000 } = process.env;
 
-app.use(express.static("./build"));
 
-app.get("/", (req, res) => {
-  res.send({ message: "server is working!" });
-})
+// app.use(express.static("./build"));
 
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`)
-})
+// app.get("/", (req, res) => {
+//   res.send({ message: "server is working!" });
+// })
+
+// app.listen(PORT, () => {
+//   console.log(`listening on port ${PORT}`)
+// })
