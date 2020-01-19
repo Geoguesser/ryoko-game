679206cbb4ca96ddb65b49cd53b4268cb81c8622
DanceParty <keevandance@gmail.com>
2020-01-18 23:10:04 -0800

    add isauthenticated middleware

--- packages/server/src/middleware/authentication.ts
+++ packages/server/src/middleware/authentication.ts
@@ -1,3 +1,16 @@
 import { Request, Response, NextFunction } from 'express'
 
-export function isAuthenticated(req: Request, res: Response, next: NextFunction) {}
+export function isAuthenticated(req: Request, res: Response, next: NextFunction) {
+  if (req.user) {
+    next()
+  } else {
+    if (req.session) {
+      // user does not exist so clear the session
+      req.session.destroy(() => null)
+    } else {
+      return res.status(401).json({
+        error: 'user is not authenticated',
+      })
+    }
+  }
+}
